b2fd1d4ffa528924bee87abd1d86a4c3a93bbf75
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-15 11:11:08 +0000

    Complete with break and continue statements.

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
     // while loop: 
-    $i = 1;
-    while($i < 101){
-        if($i % 3 === 0 && $i % 5 === 0){
-            echo "FizzBuzz\n"; 
-        }elseif($i % 3 === 0){
-            echo "Fizz\n"; 
-        }elseif($i % 5 === 0){
-            echo "Buzz"; 
-        }else{
-            echo "$i\n";
-        }
-        $i++;
-    }
+    // $i = 1;
+    // while($i < 101){
+    //     if($i % 3 === 0 && $i % 5 === 0){
+    //         echo "FizzBuzz\n"; 
+    //     }elseif($i % 3 === 0){
+    //         echo "Fizz\n"; 
+    //     }elseif($i % 5 === 0){
+    //         echo "Buzz"; 
+    //     }else{
+    //         echo "$i\n";
+    //     }
+    //     $i++;
+    // }
     
 
 //for and foreach: 
@@ -30,8 +30,18 @@
         }
     }
 
+    // foreach($output as $item){
+    //     echo "$item\n";
+    // }
+
+
+    //Break and continue: 
     foreach($output as $item){
+        if($item === "Fizz"){
+            continue;
+        }elseif($item === "FizzBuzz"){
+            echo "$item\n";
+        break;
+        }
         echo "$item\n";
     }
-
-    
