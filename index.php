fedf9ba314d2aa43bc643d090773c1837098d77e
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-15 11:03:44 +0000

    Complete with for and foreach loop.

--- index.php
+++ index.php
@@ -15,4 +15,23 @@
     }
     
 
+//for and foreach: 
+    $output = []; 
 
+    for($i=1; $i<101; $i++){
+        if($i % 3 === 0 && $i % 5 === 0){
+            array_push($output, "FizzBuzz"); 
+        }elseif($i % 3 === 0){
+            array_push($output, "Fizz");
+        }elseif($i % 5 === 0){
+            array_push($output, "Buzz");
+        }else{
+            array_push($output, "$i");
+        }
+    }
+
+    foreach($output as $item){
+        echo "$item\n";
+    }
+
+    
